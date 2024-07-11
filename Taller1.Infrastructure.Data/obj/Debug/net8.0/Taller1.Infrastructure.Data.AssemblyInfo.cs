66402816b58e5c49a711bd0a0ed2a901cb7130ae
Edison García <edison.garcia@sofka.com.co>
2024-07-11 09:58:34 -0500

    Refactor

--- Taller1.Infrastructure.Data/obj/Debug/net8.0/Taller1.Infrastructure.Data.AssemblyInfo.cs
+++ Taller1.Infrastructure.Data/obj/Debug/net8.0/Taller1.Infrastructure.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taller1.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a779c7fb4bed4cbb699c79bb11484005b346fab8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2615a4712db53ee9638dd6a10a0abc2d7a2e79b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taller1.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taller1.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Taller1.Tests/obj/Debug/net8.0/Taller1.Tests.AssemblyInfo.cs
+++ Taller1.Tests/obj/Debug/net8.0/Taller1.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taller1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a779c7fb4bed4cbb699c79bb11484005b346fab8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2615a4712db53ee9638dd6a10a0abc2d7a2e79b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taller1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taller1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
